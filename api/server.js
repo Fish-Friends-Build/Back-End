bffcfe6053a09c462ac3b6c23de72d2f383e17d0
AceIsHuman <menoman1995@yahoo.com>
2019-12-18 22:37:19 -0600

    connect api-router

--- api/server.js
+++ api/server.js
@@ -3,10 +3,14 @@ const cors = require('cors');
 const helmet = require('helmet');
 require('dotenv').config();
 
+const apiRouter = require('./api-router.js');
+
 const server = express();
 
 server.use(express.json());
 server.use(cors());
 server.use(helmet());
+
+server.use('/api', apiRouter);
 
 module.exports = server;
