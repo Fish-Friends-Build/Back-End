76dfe8eaf390e3a1b8aeed63f3245cec6090939c
AceIsHuman <menoman1995@yahoo.com>
2019-12-19 13:42:46 -0600

    create login endpoint

--- api/auth/authRouter.js
+++ api/auth/authRouter.js
@@ -18,6 +18,22 @@ router.use('/register', verifyUserFields, async (req, res) => {
     }
 });
 
+router.use('/login', verifyUserFields, async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const [user] = await Users.findBy({ username });
+        if (user && bcrypt.compareSync(password, user.password)) {
+            return res.status(200).json({
+                id: user.id,
+                username: user.username,
+                token: genToken(user)
+            })
+        }
+    } catch(err) {
+        res.status(401).json({ message: "Invalid credentials." });
+    }
+});
+
 function verifyUserFields(req, res, next) {
     const { username, password } = req.body;
     if (!username && !password) return res.status(401).json({ message: "Username and password required" });
