c6bfa477619fd4abb30718c0fc3782c29a8cd6a0
AceIsHuman <menoman1995@yahoo.com>
2019-12-19 21:54:28 -0600

    use error handler

--- api/server.js
+++ api/server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 require('dotenv').config();
+require('express-async-errors');
 
+const errorHandler = require('../utils/errorHandler.js');
 const apiRouter = require('./api-router.js');
 
 const server = express();
@@ -12,5 +14,13 @@ server.use(cors());
 server.use(helmet());
 
 server.use('/api', apiRouter);
+
+server.use((req, res, next) => {
+  const error = new Error('Invalid endpoint');
+  error.status = 404;
+  next(error);
+});
+
+server.use(errorHandler);
 
 module.exports = server;
