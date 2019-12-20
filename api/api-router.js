95408acabb268c353c508935f995d7f687e68db6
AceIsHuman <menoman1995@yahoo.com>
2019-12-19 21:55:12 -0600

    connect journals router

--- api/api-router.js
+++ api/api-router.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const authRouter = require('./auth/authRouter.js');
+const journalsRouter = require('./journals/journalsRouter.js');
 
 router.use('/auth', authRouter);
+router.use('/journals', journalsRouter);
 
 module.exports = router;
