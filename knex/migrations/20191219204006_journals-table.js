13f3777d264784ee05c12f8f519316b998ebc957
AceIsHuman <menoman1995@yahoo.com>
2019-12-19 21:04:59 -0600

    create journals table

---
+++ knex/migrations/20191219204006_journals-table.js
@@ -0,0 +1,25 @@
+
+exports.up = function(knex) {
+  return (
+    knex.schema
+      .createTable('journals', tbl => {
+        tbl.increments();
+        tbl.integer('numFishCaught').notNullable();
+        tbl.date('date').notNullable();
+        tbl.string('timeOfDay').notNullable();
+        tbl.string('location').notNullable();
+        tbl.string('fishType');
+        tbl.string('bait');
+        tbl.text('bankOrBoat').notNullable();
+        tbl.text('waterType').notNullable();
+        tbl.string('notes');
+      })
+  );
+};
+
+exports.down = function(knex) {
+  return (
+    knex.schema
+      .dropTableIfExists('jounals')
+  );
+};
