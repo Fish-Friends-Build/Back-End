769645623696434e362d4960960936da6be83577
AceIsHuman <menoman1995@yahoo.com>
2019-12-19 22:12:13 -0600

    use restricted middleware on journalsRouter

--- api/api-router.js
+++ api/api-router.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
+const restricted = require('../utils/restricted.js');
+
 const authRouter = require('./auth/authRouter.js');
 const journalsRouter = require('./journals/journalsRouter.js');
 
 router.use('/auth', authRouter);
-router.use('/journals', journalsRouter);
+router.use('/journals', restricted, journalsRouter);
 
 module.exports = router;
