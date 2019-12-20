b9abfb52ba95821ab658c5420b47ee5370d3398b
AceIsHuman <menoman1995@yahoo.com>
2019-12-19 21:09:50 -0600

    create journals helpers

---
+++ api/journals/journalsModel.js
@@ -0,0 +1,31 @@
+const db = require('../../knex/dbConfig.js');
+
+module.exports = {
+  find,
+  findBy,
+  insert,
+  update,
+  remove
+};
+
+function find() {
+  return db('jounals');
+}
+
+function findBy(id) {
+  return db('journals').where(id);
+}
+
+function insert(journal) {
+  return db('journals').insert(journal, ['*']);
+}
+
+function update(id, update) {
+  return db('journals')
+    .where({ id })
+    .update(update, ['*']);
+}
+
+function remove(id) {
+  return db('journals').where({ id }).del();
+}
