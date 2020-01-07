067596d0adccd6cae9fadc27245c6f14c38ede27
AceIsHuman <menoman1995@yahoo.com>
2020-01-06 19:39:36 -0600

    return error if fishType is not an array

--- api/journals/journalsRouter.js
+++ api/journals/journalsRouter.js
@@ -3,6 +3,12 @@ const Journals = require('./journalsModel.js');
 
 router.post('/', async (req, res) => {
   const user = res.locals.decodedJwt;
+  const fishType = req.body.fishType;
+  if (fishType) {
+    if (!Array.isArray(fishType)) {
+      return res.status(401).json({ message: 'Value of fishType must be an array.' })
+    }
+  }
   const [journal] = await Journals.insert({ ...req.body, userId: user.id });
   return res.status(200).json(journal);
 });
