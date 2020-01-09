db5c3016d82df955b25b1596ec8b6b0a827b0742
AceIsHuman <menoman1995@yahoo.com>
2020-01-08 20:43:45 -0600

    create environment for testing

--- knexfile.js
+++ knexfile.js
@@ -17,6 +17,23 @@ module.exports = {
     },
     useNullAsDefault: true
   },
+  
+  testing: {
+    client: 'pg',
+    connection: {
+      host: process.env.PG_HOST,
+      user: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DATABASE_TESTING
+    },
+    migrations: {
+      directory: './knex/migrations'
+    },
+    seeds: {
+      directory: './knex/seeds'
+    },
+    useNullAsDefault: true
+  },
 
   production: {
     client: 'pg',
