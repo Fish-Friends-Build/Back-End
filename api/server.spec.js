bc52bdc46c619d119ee51cbdc6f569ecf2e49731
AceIsHuman <menoman1995@yahoo.com>
2020-01-09 23:20:18 -0600

    test add journal endpoint

--- api/server.spec.js
+++ api/server.spec.js
@@ -42,6 +42,31 @@ describe('LOGIN USER: POST: /api/auth/login', () => {
       .send({ username: 'username', password: 'nothatone' })
       .set('Content-Type', 'application/json');
     expect(res.status).toBe(401);
-    expect(res.body.token).toBeNull();
+    expect(res.body.token).toBeUndefined();
+  });
+});
+
+describe('ADD JOURNAL ENTRY: POST: /api/journals', () => {
+  it('should return 200 and contain username', async () => {
+    const login = await request(server)
+      .post('/api/auth/login')
+      .send({ username: 'username', password: 'password' })
+      .set('Content-Type', 'application/json');
+      
+      const res = await request(server)
+      .post('/api/journals')
+      .send({
+        numFishCaught: 4,
+        date: 'Jan 9th, 2020',
+        timeOfDay: 'Morning',
+        location: 'Hickory Creek',
+        bankOrBoat: 'bank',
+        waterType: 'freshwater'
+      })
+      .set('Content-Type', 'application/json')
+      .set('Authorization', login.body.token);
+
+    expect(res.status).toBe(200);
+    expect(res.body.username).toBeTruthy();
   });
 });
